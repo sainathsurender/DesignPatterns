4a9a2749cdabe934c561e774b25395049c53c83e
sainathsurender <36662223+sainathsurender@users.noreply.github.com>
2025-01-16 22:22:26 +0530

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,15 @@
+public sealed class Singleton
+{
+    private static readonly Lazy<Singleton> _instance = 
+        new Lazy<Singleton>(() => new Singleton());
+
+    // Private constructor to prevent instantiation
+    private Singleton() { }
+
+    public static Singleton Instance => _instance.Value;
+
+    public void DisplayMessage()
+    {
+        Console.WriteLine("Singleton instance method called.");
+    }
+}
